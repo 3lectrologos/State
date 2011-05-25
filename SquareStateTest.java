a377a88b258266ea67ad80dd915a7dea6e2b442d
Alkis Gotovos <el3ctrologos@hotmail.com>
2011-05-25 21:08:15 +0300

    Add a one-move sliding square test

--- SquareStateTest.java
+++ SquareStateTest.java
@@ -56,9 +56,9 @@ public class SquareStateTest extends TestCase {
 	}
 	
 	public void testNext() {
-		SquaresState state1 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
-		SquaresState state2 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 0}, {7, 8, 6}});
-		SquaresState state3 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}});
+		State state1 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+		State state2 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 0}, {7, 8, 6}});
+		State state3 = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}});
 
 		Set<State> set = state1.nextStates();
 		set.remove(state2);
@@ -67,11 +67,11 @@ public class SquareStateTest extends TestCase {
 	}
 	
 	public void testNext2() {
-		SquaresState state1 = new SquaresState(new int[][] {{3, 1, 4}, {7, 0, 8}, {2, 6, 5}});
-		SquaresState state2 = new SquaresState(new int[][] {{3, 1, 4}, {0, 7, 8}, {2, 6, 5}});
-		SquaresState state3 = new SquaresState(new int[][] {{3, 0, 4}, {7, 1, 8}, {2, 6, 5}});
-		SquaresState state4 = new SquaresState(new int[][] {{3, 1, 4}, {7, 8, 0}, {2, 6, 5}});
-		SquaresState state5 = new SquaresState(new int[][] {{3, 1, 4}, {7, 6, 8}, {2, 0, 5}});
+		State state1 = new SquaresState(new int[][] {{3, 1, 4}, {7, 0, 8}, {2, 6, 5}});
+		State state2 = new SquaresState(new int[][] {{3, 1, 4}, {0, 7, 8}, {2, 6, 5}});
+		State state3 = new SquaresState(new int[][] {{3, 0, 4}, {7, 1, 8}, {2, 6, 5}});
+		State state4 = new SquaresState(new int[][] {{3, 1, 4}, {7, 8, 0}, {2, 6, 5}});
+		State state5 = new SquaresState(new int[][] {{3, 1, 4}, {7, 6, 8}, {2, 0, 5}});
 
 		Set<State> set = state1.nextStates();
 		set.remove(state2);
@@ -79,5 +79,12 @@ public class SquareStateTest extends TestCase {
 		set.remove(state4);
 		set.remove(state5);
 		assertTrue(set.isEmpty());
+	}
+	
+	public void testSolve() {
+		State initState = new SquaresState(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}});
+		Solver solver = new BFSSolver();
+		
+		System.out.println(solver.solve(initState));
 	}
 }
